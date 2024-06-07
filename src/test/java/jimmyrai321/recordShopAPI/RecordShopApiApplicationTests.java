2354281882faf8cc34fa14d3de84f5194bea307c
Jimmyrai321 <jimmyrai321@gmail.com>
2024-06-07 10:33:45 +0100

    inital commit

---
+++ src/test/java/jimmyrai321/recordShopAPI/RecordShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package jimmyrai321.recordShopAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
